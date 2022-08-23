125d9b2f3c2dd8a74ba2796510d88ce65b138219
parvezaman <emailtoamanullah@gmail.com>
2022-08-23 16:37:43 +0600

    added context api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './context/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,13 +17,13 @@ function App() {
     setCartIsShown(false);
   }
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} /> }
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../context/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+        return curNumber + item.amount;
+    }, 0);
+    
     return ( 
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -12,7 +19,7 @@ const HeaderCartButton = (props) => {
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
