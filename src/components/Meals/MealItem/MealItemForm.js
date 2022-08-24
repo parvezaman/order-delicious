ae6e0fa67cc54643f456c0b3ec4788f14d03b223
parvezaman <emailtoamanullah@gmail.com>
2022-08-24 12:05:36 +0600

    aded ref and useref to update cart badge amount

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-const MealItemForm = () => {
-    const submitHandler = event =>{
+const MealItemForm = (props) => {
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const submitHandler = event => {
         event.preventDefault();
+        console.log("Clicked");
+
+        const enteredAmount = amountInputRef.current.value; //this value will always be a string
+        const enteredAmountNumber = +enteredAmount; //so converting the string into number
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNumber);
     }
+
     return (
         <form className={classes.form} onSubmit={submitHandler}>
-            <Input label="Amount" input={{
-                id: "amount",
-                type: "number",
-                min: "1",
-                max: "5",
-                step: "1",
-                defaultValue: "1"
-            }} /> 
+            <Input
+                ref={amountInputRef}
+                label="Amount"
+                input={{
+                    id: "amount",
+                    type: "number",
+                    min: "1",
+                    max: "5",
+                    step: "1",
+                    defaultValue: "1"
+                }} />
             <button>+ ADD</button>
+            {!amountIsValid && <p>Please input a valid amount (1-5)</p>}
         </form>
     );
 };
